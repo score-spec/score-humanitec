fa86a001a3679637fd4623af7579c16e05206f19
Eugene Yarshevich <yarshevich@gmail.com>
2022-12-21 01:45:10 +1100

    feat: added metadata properties substitutions support
    Signed-off-by: Eugene Yarshevich <yarshevich@gmail.com>

---
+++ internal/humanitec/templates.go
@@ -0,0 +1,118 @@
+/*
+Apache Score
+Copyright 2022 The Apache Software Foundation
+
+This product includes software developed at
+The Apache Software Foundation (http://www.apache.org/).
+*/
+package humanitec
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mitchellh/mapstructure"
+
+	score "github.com/score-spec/score-go/types"
+	extensions "github.com/score-spec/score-humanitec/internal/humanitec/extensions"
+)
+
+// templatesContext ia an utility type that provides a context for '${...}' templates substitution
+type templatesContext map[string]string
+
+// buildContext initializes a new templatesContext instance
+func buildContext(metadata score.WorkloadMeta, resources score.ResourcesSpecs, ext extensions.HumanitecResourcesSpecs) (templatesContext, error) {
+	var ctx = make(map[string]string)
+
+	var metadataMap = make(map[string]interface{})
+	if decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		TagName: "json",
+		Result:  &metadataMap,
+	}); err != nil {
+		return nil, err
+	} else {
+		decoder.Decode(metadata)
+		for key, val := range metadataMap {
+			var ref = fmt.Sprintf("metadata.%s", key)
+			if _, exists := ctx[ref]; exists {
+				return nil, fmt.Errorf("ambiguous property reference '%s'", ref)
+			}
+			ctx[ref] = fmt.Sprintf("%v", val)
+		}
+	}
+
+	for resName, res := range resources {
+		var source string
+		switch res.Type {
+		case "environment":
+			source = "values"
+		case "workload":
+			source = fmt.Sprintf("modules.%s", resName)
+		default:
+			if resExt, exists := ext[resName]; exists && resExt.Scope == "shared" {
+				source = fmt.Sprintf("shared.%s", resName)
+			} else {
+				source = fmt.Sprintf("externals.%s", resName)
+			}
+		}
+		ctx[fmt.Sprintf("resources.%s", resName)] = source
+
+		for propName := range res.Properties {
+			var ref = fmt.Sprintf("resources.%s.%s", resName, propName)
+			if _, exists := ctx[ref]; exists {
+				return nil, fmt.Errorf("ambiguous property reference '%s'", ref)
+			}
+			ctx[ref] = fmt.Sprintf("${%s.%s}", source, propName)
+		}
+	}
+
+	return ctx, nil
+}
+
+// SubstituteAll replaces all matching '${...}' templates in map keys and string values recursively.
+func (context templatesContext) SubstituteAll(src map[string]interface{}) map[string]interface{} {
+	var dst = make(map[string]interface{}, 0)
+
+	for key, val := range src {
+		key = context.Substitute(key)
+		switch v := val.(type) {
+		case string:
+			val = context.Substitute(v)
+		case map[string]interface{}:
+			val = context.SubstituteAll(v)
+		}
+		dst[key] = val
+	}
+
+	return dst
+}
+
+// Substitute replaces all matching '${...}' templates in a source string
+func (context templatesContext) Substitute(src string) string {
+	return os.Expand(src, context.mapVar)
+}
+
+// MapVar replaces objects and properties references with corresponding values
+// Returns an empty string if the reference can't be resolved
+func (context templatesContext) mapVar(ref string) string {
+	if ref == "" {
+		return ""
+	}
+
+	// NOTE: os.Expand(..) would invoke a callback function with "$" as an argument for escaped sequences.
+	//       "$${abc}" is treated as "$$" pattern and "{abc}" static text.
+	//       The first segment (pattern) would trigger a callback function call.
+	//       By returning "$" value we would ensure that escaped sequences would remain in the source text.
+	//       For example "$${abc}" would result in "${abc}" after os.Expand(..) call.
+	if ref == "$" {
+		return ref
+	}
+
+	if res, ok := context[ref]; ok {
+		return res
+	}
+
+	log.Printf("Warning: Can not resolve '%s'. Resource or property is not declared.", ref)
+	return fmt.Sprintf("${%s}", ref)
+}
---
+++ internal/humanitec/templates_test.go
@@ -0,0 +1,192 @@
+/*
+Apache Score
+Copyright 2022 The Apache Software Foundation
+
+This product includes software developed at
+The Apache Software Foundation (http://www.apache.org/).
+*/
+package humanitec
+
+import (
+	"testing"
+
+	score "github.com/score-spec/score-go/types"
+	assert "github.com/stretchr/testify/assert"
+
+	extensions "github.com/score-spec/score-humanitec/internal/humanitec/extensions"
+)
+
+func TestBuildContext(t *testing.T) {
+	var meta = score.WorkloadMeta{
+		Name: "test-name",
+	}
+
+	var resources = score.ResourcesSpecs{
+		"env": score.ResourceSpec{
+			Type: "environment",
+			Properties: map[string]score.ResourcePropertySpec{
+				"DEBUG": {Required: false, Default: true},
+			},
+		},
+		"db": score.ResourceSpec{
+			Type: "postgres",
+			Properties: map[string]score.ResourcePropertySpec{
+				"host": {Required: true, Default: "."},
+				"port": {Required: true, Default: "5342"},
+				"name": {Required: true},
+			},
+		},
+		"dns": score.ResourceSpec{
+			Type: "dns",
+			Properties: map[string]score.ResourcePropertySpec{
+				"domain": {},
+			},
+		},
+		"service-a": score.ResourceSpec{
+			Type: "workload",
+			Properties: map[string]score.ResourcePropertySpec{
+				"service.name": {},
+			},
+		},
+	}
+
+	var ext = extensions.HumanitecResourcesSpecs{
+		"dns": {Scope: "shared"},
+	}
+
+	context, err := buildContext(meta, resources, ext)
+	assert.NoError(t, err)
+
+	assert.Equal(t, templatesContext{
+		"metadata.name": "test-name",
+
+		"resources.env":       "values",
+		"resources.env.DEBUG": "${values.DEBUG}",
+
+		"resources.db":      "externals.db",
+		"resources.db.host": "${externals.db.host}",
+		"resources.db.port": "${externals.db.port}",
+		"resources.db.name": "${externals.db.name}",
+
+		"resources.dns":        "shared.dns",
+		"resources.dns.domain": "${shared.dns.domain}",
+
+		"resources.service-a":              "modules.service-a",
+		"resources.service-a.service.name": "${modules.service-a.service.name}",
+	}, context)
+}
+
+func TestMapVar(t *testing.T) {
+	var context = templatesContext{
+		"metadata.name": "test-name",
+
+		"resources.env":       "values",
+		"resources.env.DEBUG": "${values.DEBUG}",
+
+		"resources.db":      "externals.db",
+		"resources.db.host": "${externals.db.host}",
+		"resources.db.port": "${externals.db.port}",
+		"resources.db.name": "${externals.db.name}",
+
+		"resources.dns":        "shared.dns",
+		"resources.dns.domain": "${shared.dns.domain}",
+
+		"resources.service-a":              "modules.service-a",
+		"resources.service-a.service.name": "${modules.service-a.service.name}",
+	}
+
+	assert.Equal(t, "", context.mapVar(""))
+	assert.Equal(t, "$", context.mapVar("$"))
+
+	assert.Equal(t, "test-name", context.mapVar("metadata.name"))
+	assert.Equal(t, "${metadata.name.nil}", context.mapVar("metadata.name.nil"))
+	assert.Equal(t, "${metadata.nil}", context.mapVar("metadata.nil"))
+
+	assert.Equal(t, "${values.DEBUG}", context.mapVar("resources.env.DEBUG"))
+
+	assert.Equal(t, "externals.db", context.mapVar("resources.db"))
+	assert.Equal(t, "${externals.db.host}", context.mapVar("resources.db.host"))
+	assert.Equal(t, "${externals.db.port}", context.mapVar("resources.db.port"))
+	assert.Equal(t, "${externals.db.name}", context.mapVar("resources.db.name"))
+	assert.Equal(t, "${resources.db.name.nil}", context.mapVar("resources.db.name.nil"))
+	assert.Equal(t, "${resources.db.nil}", context.mapVar("resources.db.nil"))
+	assert.Equal(t, "${resources.nil}", context.mapVar("resources.nil"))
+	assert.Equal(t, "${nil.db.name}", context.mapVar("nil.db.name"))
+}
+
+func TestSubstitute(t *testing.T) {
+	var context = templatesContext{
+		"metadata.name": "test-name",
+
+		"resources.env":       "values",
+		"resources.env.DEBUG": "${values.DEBUG}",
+
+		"resources.db":      "externals.db",
+		"resources.db.host": "${externals.db.host}",
+		"resources.db.port": "${externals.db.port}",
+		"resources.db.name": "${externals.db.name}",
+
+		"resources.dns":        "shared.dns",
+		"resources.dns.domain": "${shared.dns.domain}",
+
+		"resources.service-a":              "modules.service-a",
+		"resources.service-a.service.name": "${modules.service-a.service.name}",
+	}
+
+	assert.Equal(t, "", context.Substitute(""))
+	assert.Equal(t, "abc", context.Substitute("abc"))
+	assert.Equal(t, "abc $ abc", context.Substitute("abc $$ abc"))
+	assert.Equal(t, "${abc}", context.Substitute("$${abc}"))
+
+	assert.Equal(t, "The name is 'test-name'", context.Substitute("The name is '${metadata.name}'"))
+	assert.Equal(t, "The name is '${metadata.nil}'", context.Substitute("The name is '${metadata.nil}'"))
+
+	assert.Equal(t, "resources.env.DEBUG", context.Substitute("resources.env.DEBUG"))
+
+	assert.Equal(t, "externals.db", context.Substitute("${resources.db}"))
+	assert.Equal(t,
+		"postgresql://${resources.db.user}:${resources.db.password}@${externals.db.host}:${externals.db.port}/${externals.db.name}",
+		context.Substitute("postgresql://${resources.db.user}:${resources.db.password}@${resources.db.host}:${resources.db.port}/${resources.db.name}"))
+}
+
+func TestMapAllVars(t *testing.T) {
+	var context = templatesContext{
+		"metadata.name": "test-name",
+
+		"resources.env":       "values",
+		"resources.env.DEBUG": "${values.DEBUG}",
+
+		"resources.db":      "externals.db",
+		"resources.db.host": "${externals.db.host}",
+		"resources.db.port": "${externals.db.port}",
+		"resources.db.name": "${externals.db.name}",
+
+		"resources.dns":        "shared.dns",
+		"resources.dns.domain": "${shared.dns.domain}",
+
+		"resources.service-a":              "modules.service-a",
+		"resources.service-a.service.name": "${modules.service-a.service.name}",
+	}
+
+	var source = map[string]interface{}{
+		"api": map[string]interface{}{
+			"${resources.service-a.service.name}": map[string]interface{}{
+				"url":  "http://${resources.dns.domain}",
+				"port": 80,
+			},
+		},
+		"DEBUG": "${resources.env.DEBUG}",
+	}
+
+	var expected = map[string]interface{}{
+		"api": map[string]interface{}{
+			"${modules.service-a.service.name}": map[string]interface{}{
+				"url":  "http://${shared.dns.domain}",
+				"port": 80,
+			},
+		},
+		"DEBUG": "${values.DEBUG}",
+	}
+
+	assert.Equal(t, expected, context.SubstituteAll(source))
+}
